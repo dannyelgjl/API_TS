a4d25dfb4a1fe0606ab97b95a8d19ae84ca00917
dannyelgjl <dannyelgjl@gmail.com>
2020-03-24 12:45:56 -0300

    estrutura do projeto ok

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import routes from "./routes";
+import cors from "cors";
+
+import "./database";
+
+class App {
+  public server: express.Application;
+
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  private middlewares(): void {
+    this.server.use(express.json());
+    this.server.use(cors());
+  }
+
+  private routes(): void {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
