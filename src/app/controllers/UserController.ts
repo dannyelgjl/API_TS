0559a7a05250d6bcdfee3dfb456bd003353f561f
dannyelgjl <dannyelgjl@gmail.com>
2020-03-24 15:41:54 -0300

    end-point update

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -14,6 +14,32 @@ class UserController {
 
     return res.json(user);
   }
+
+  async update(req: Request, res: Response): Promise<Response> {
+    const user = await User.findById({
+      _id: req.params.id
+    });
+
+    const { nome, idade, email } = req.body;
+
+    await user?.updateOne({
+      nome,
+      idade,
+      email
+    });
+
+    console.log({
+      nome: nome,
+      idade: idade,
+      email: email
+    });
+
+    return res.json({
+      nome,
+      idade,
+      email
+    });
+  }
 }
 
 export default new UserController();
